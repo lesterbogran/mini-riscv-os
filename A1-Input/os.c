e4e3bc1bc15c909c66ec102174c55096f594b4c5
lesterbogran <lesterbogran@yahoo.com>
2023-04-05 15:51:27 -0600

    Update os.c

--- A1-Input/os.c
+++ A1-Input/os.c
@@ -1,24 +1,24 @@
 #include <stdint.h>
 
-#define UART        0x10000000            // RISC-V 的 virt 虛擬機，其 UART 映射區起始位址為 0x10000000
-#define UART_THR    (uint8_t*)(UART+0x00) // THR:transmitter holding register 傳送暫存器
-#define UART_RHR    (uint8_t*)(UART+0x00) // THR:transmitter holding register 傳送暫存器
-#define UART_LSR    (uint8_t*)(UART+0x05) // LSR:line status register 輸出狀態暫存器
-#define UART_LSR_RX_READY 0x01            // input is waiting to be read from RHR
-#define UART_LSR_EMPTY_MASK 0x40          // LSR Bit 6: 當 LSR 的第六位元為 1 時，代表傳送區為空的，可以傳了 (Transmitter empty; both the THR and LSR are empty)
+#define UART        0x10000000            // La máquina virtual virt de RISC-V tiene la dirección base de mapeo del UART en 0x10000000.
+#define UART_THR    (uint8_t*)(UART+0x00) // THR: transmitter holding register
+#define UART_RHR    (uint8_t*)(UART+0x00) // RHR: receiving holding register 
+#define UART_LSR    (uint8_t*)(UART+0x05) // LSR: line status register 
+#define UART_LSR_RX_READY 0x01            // Hay entrada esperando ser leída desde RHR.
+#define UART_LSR_EMPTY_MASK 0x40          // LSR Bit 6: Cuando el sexto bit de LSR es 1, significa que el área de transmisión está vacía y puede ser usada para transmitir.
 
-void lib_putc(char ch) { // 透過 UART 傳送字元 ch 給宿主機印出
-	while ((*UART_LSR & UART_LSR_EMPTY_MASK) == 0); // 一直等到 UART LSR 傳送區為空，可以傳送了 
-	*UART_THR = ch; // 將字元 ch 發送出去
+void lib_putc(char ch) {  // Este código está enviando el carácter 'ch' a través del UART para que se imprima en la máquina anfitriona.
+	while ((*UART_LSR & UART_LSR_EMPTY_MASK) == 0); // El código está esperando a que el LSR del UART indique que el registro de transmisión está vacío para poder enviar el carácter.
+	*UART_THR = ch; // El carácter ch es enviado.
 }
 
-void lib_puts(char *s) { // 印出字串到宿主機。
+void lib_puts(char *s) { // Imprimir una cadena en la máquina anfitriona.
 	while (*s) lib_putc(*s++);
 }
 
 char lib_getc() {
-	while ((*UART_LSR & UART_LSR_RX_READY) == 0); // 一直等到 UART RX_READY 有收到資料 
-	return *UART_RHR; // 傳回收到的字元
+	while ((*UART_LSR & UART_LSR_RX_READY) == 0); // Espera continuamente hasta que el UART reciba datos (RX_READY).
+	return *UART_RHR; // Devuelve el carácter recibido
 }
 
 int lib_gets(char *s) {
@@ -41,6 +41,6 @@ int os_main(void)
 	lib_puts("\r\nyou type:");
 	lib_puts(str);
 	lib_puts("\r\n");
-	while (1) {} // 讓主程式卡在這裡，用無窮迴圈停止！
+	while (1) {} // ¡Hacer que el programa principal se quede aquí, detenido en un bucle infinito!
 	return 0;
 }
